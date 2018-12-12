7db9c1764d68fe7106bd80ac932af8a0741e8584
baakash1998 <baakash1998@gmail.com>
2018-12-12 11:18:38 +0530

    12/12/18

--- Hong Kong Tour/Sumiran Pullan/MyJSLibrary.js
+++
@@ -1,5 +0,0 @@
-function ShowMessage() {
-	newwin = window.open("JS6.html", "win","menubar=no,scrollbars=yes,width=400,height=500,location=no,resizable=yes")
-    newwin.focus()
-    setTimeout("newwin.close()",5000)
-}
